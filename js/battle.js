2273ec3cebe0b20b0a6f85dda7894af325f65eca
not-jay <jaysonsona@tfwno.gf>
2015-03-15 22:18:40 +0800

    Added updateUI() and issue() to battle.js

--- js/battle.js
+++ js/battle.js
@@ -28,6 +28,58 @@ var Battle = function() {
 		ui.player.field = ui_element.fp;
 		ui.player.stats = ui_element.sp;
 		ui.enemy = ui_element.fe;
+		this.updateUI();
+	}
+
+	this.updateUI = function() {
+		var player_unit, enemy_unit, i,
+			player_field, player_avatar;
+
+		for (i = 0; i < 4; i++) {
+			player_unit = player.get(i);
+			enemy_unit = enemy.get(i);
+
+			if(player_unit !== null) {
+				player_field = String.format(ui.player.field, i);
+				player_avatar = String.format(ui.player.stats, i);
+
+				$(String.format("#{0}", player_field)).css(
+					"background-image", String.format("url({0})", player_unit.sprite[player_unit.state])
+				);
+				$(String.format("#{0} .avatar", player_avatar)).css(
+					"background-image", String.format("url({0})", player_unit.avatar)
+				);
+				$(String.format("#{0} .name", player_avatar)).html(
+					player_unit.name
+				);
+				$(String.format("#{0} .health", player_avatar)).html(
+					player_unit.hp+" / "+player_unit.max_hp
+				);
+			}
+			if(enemy_unit !== null) {
+				$(String.format("#"+ui.enemy, i)).css(
+					"background-image", String.format("url{0})", enemy_unit.sprite.idle)
+				);
+			}
+		};
+	}
+
+	this.issue = function(command) {
+		var i, field, unit,
+			length = player.size();
+
+		for(i = 0; i < length; i++) {
+			field = String.format(ui.player.field, i);
+			unit = player.get(i);
+
+			if(command === "retreat") {
+				unit.state = command;
+				$(String.format("#{0}", field)).addClass("retreating");
+			} else {
+				unit.state = "idle";
+				$(String.format("#{0}", field)).removeClass("retreating");
+			}
+		}
 	}
 
 	this.player = function() { return player; }
