7b5b7575867a11b9639c4946ded4dbb98fb716bd
not-jay <jaysonsona@tfwno.gf>
2015-03-15 21:55:26 +0800

    Added acknowledgements

--- js/string.extended.js
+++ js/string.extended.js
@@ -1,3 +1,10 @@
+// This basically extends the native String
+// adding a format and endsWith functionality
+
+/**
+ *	fearphage's String.format()
+ *	http://stackoverflow.com/a/4673436/1135805
+ */
 if(!String.format) {
 	String.format = function(format) {
 		var args = Array.prototype.slice.call(arguments, 1);
@@ -7,6 +14,10 @@ if(!String.format) {
 	};
 }
 
+/**
+ *	chakrit's String.prototype.endsWith()
+ *	http://stackoverflow.com/a/2548133/1135805
+ */
 if(!String.prototype.endsWith) {
 	String.prototype.endsWith = function(suffix) {
 		return this.indexOf(suffix, this.length - suffix.length) !== -1;
