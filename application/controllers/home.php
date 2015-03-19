ef51f6219d8977789b4b1e1d23c15328d575483c
not-jay <jaysonsona@tfwno.gf>
2015-03-20 04:53:26 +0800

    Added login/register

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,10 +4,65 @@ class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
+
+		$this->load->model("user_model", "users");
+		$this->load->helper("form");
 	}
 
 	public function index() {
-		$this->load->view("home/login_view");
+		if($this->session->userdata("logged_in")):
+			redirect("town");
+		else:
+			$this->load->view("home/login_view");
+		endif;
+	}
+
+	public function login() {
+		$user = $this->input->post("username");
+		$pass = $this->input->post("password");
+		$result = array();
+
+		if($this->users->validate($user, $pass)):
+			//login user
+			$data = $this->users->get_data($user);
+			$this->users->login($user);
+			$session_data = array(
+				"id"		=> $data["id"],
+				"last_login"=> $data["last_login"],
+				"username"	=> $user,
+				"logged_in"	=> true
+			);
+			$this->session->set_userdata($session_data);
+			$result["status"] = "okay";
+		else:
+			$result["status"] = "error";
+			$result["error"] = "Invalid username or password";
+		endif;
+
+		echo json_encode($result);
+	}
+
+	public function register() {
+		$user = $this->input->post("username");
+		$pass = $this->input->post("password");
+		$result = array();
+
+		if(!$this->users->taken($user)):
+			//register
+			$this->users->register($user, $pass);
+			$result["status"] = "okay";
+			$result["info"] = "You may now log-in using your credentials";
+		else:
+			$result["status"] = "error";
+			$result["error"] = "Username is already taken";
+		endif;
+
+		echo json_encode($result);
+	}
+
+	public function logout() {
+		$this->session->sess_destroy();
+		redirect("/");
 	}
 
 }
--- application/views/home/login_view.php
+++ application/views/home/login_view.php
@@ -4,36 +4,70 @@
 	<meta charset="utf-8">
 	<title>Dungeon Crawler Mini-RPG</title>
 	<?= link_tag("css/misc.css"); ?>
+	<?= link_tag("css/rpg-ui.css"); ?>
 	<?= link_tag("css/bootstrap.min.css"); ?>
-	<style type="text/css">
-	/* I balhin lng ni into its own css */
-	.center-middle { /*Para mu work si 'height: 80%;' sa .center-content*/
-		height: 100%;
-	}
-
-	.center-content {
-		width: 80%;
-		height: 80%;
-	}
-
-	.title {
-		border: 1px dotted black;
-	}
-	</style>
+	<?= link_tag("css/bootstrap-theme.min.css"); ?>
 </head>
 <body>
 	<div class="center-outer"><div class="center-middle">
-		<div class="center-content center-text title">
-			hello
-		</div>
-		<div class="center-content center-text title">
-			<div class="two-column pull-left">
-				left
+		<div class="center-content center-block ff7 pad-me">
+			<div class="form-group form-inline">
+				<label for="username" class="control-label">Username</label>
+				<input type="text" id="username" name="username">
 			</div>
-			<div class="two-column pull-left">
-				right
+			<div class="form-group">
+				<label for="username" class="control-label">Password</label>
+				<input type="password" id="password" name="password">
+			</div>
+			<div class="form-group center-text">
+				<p id="errors"></p>
+			</div>
+			<div class="form-group center-text">
+				<button id="login" class="btn btn-default">Login</button>
+				<button id="register" class="btn btn-default">Register</button>
 			</div>
 		</div>
 	</div></div>
 </body>
+<script type="text/javascript" src="<?= base_url('js/jquery.min.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('js/bootstrap.min.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('js/base.url.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('js/string.extended.js'); ?>"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$("#login").click(function() {
+		controller("login");
+	});
+
+	$("#register").click(function() {
+		controller("register");
+	});
+})
+function controller(action) {
+	var link = (action === "login")?"home/login":"home/register";
+
+	$("#errors").hide();
+	$.ajax(link, {
+		type: "POST",
+		dataType: "json",
+		data: {
+			"username": $("#username").val(),
+			"password": $("#password").val()
+		},
+		success: function(data) {
+			if(data.status === "okay") {
+				if(action === "login") {
+					$(location).attr("href", "town");
+				} else {
+					$("#errors").html(data.info).show();
+				}
+			} else {
+				$("#errors").html(data.error).show();
+				$("#username").val("");
+				$("#password").val("");
+			}
+		}
+	});
+}
+</script>
 </html>
