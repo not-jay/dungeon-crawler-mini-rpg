d11cc740b2bdcbea88c590ac38d5c21389507bcb
not-jay <jaysonsona@tfwno.gf>
2015-03-20 04:57:29 +0800

    Updated recruit screen

--- application/views/recruit/recruit_view.php
+++ application/views/recruit/recruit_view.php
@@ -13,15 +13,16 @@
 		<div class="center-content">
 			<div class="two-column pull-left units ff7">
 				<div class="center-text title"><h1>New Units</h1></div>
-				<?php for($i = 0; $i < 4; $i++): ?>
-				<div class="media unit-slot" onClick="selectUnit(<?= $i; ?>);">
+				<?php for($i = 0; $i < count($units); $i++): ?>
+				<div id="unit-<?= $i ?>" class="media unit-slot" onClick="selectUnit(<?= $i; ?>);">
 					<div class="media-left media-middle">
 						<div class="media-object avatar">
 							<img src="<?= base_url('img/avatars/'.$units[$i]->sprite_name.'.gif'); ?>" />
 						</div>
 					</div>
 					<div class="media-body media-middle">
-						<div class="name">Name: <?= $units[$i]->name; ?></div>
+						<div id="name-<?= $i ?>"><?= $units[$i]->name; ?></div>
+						<div id="recruit-status-<?= $i ?>"></div>
 					</div>
 				</div>
 				<?php endfor; ?>
@@ -32,15 +33,16 @@
 				<div class="center-block center-content unit-stats">
 					Name: <input type="text" id="unit-name" placeholder="Enter a name">
 					<div class="spring">
-						HP: <span id="hp"></span><br>
+						<input id="id" type="hidden">
 						Level: <span id="level"></span><br>
+						HP: <span id="hp"></span><br>
 						Attack: <span id="atk"></span><br>
 						Defense: <span id="def"></span><br>
 						Evasion: <span id="evade"></span><br>
 						Speed: <span id="spd"></span><br>
 					</div>
 					<div class="spring">
-						<button class="btn btn-default center-block">Recruit</button>
+						<button id="recruit" class="btn btn-default center-block">Recruit</button>
 					</div>
 				</div>
 			</div>
@@ -60,7 +62,9 @@ $(document).ready(function() {
 	units = new Party();
 	initBaseURL("429/final_project/");
 
-	playerParty.addAll(Unit.createAll(controller("unit/get_party", "json", 1))); //TODO: replace 1
+	playerParty.addAll(Unit.createAll(
+		controller("unit/get_party", "json",<?= $this->session->userdata("id"); ?>)
+	));
 	units.addAll(Unit.createAll(JSON.parse('<?= json_encode($units); ?>')));
 	ave = playerParty.averageLevel() || 1;
 	length = units.size();
@@ -69,19 +73,42 @@ $(document).ready(function() {
 		unit = units.get(i);
 		unit.levelUp(Math.floor(Math.random() * ave) + low);
 	}
+
+	$("#recruit").click(function() {
+		var id = $("#id").val(),
+			unit = units.get(id),
+			unit_data = unit.jsonify();
+
+		delete unit_data.characters.id;
+		unit_data.characters.name = $("#unit-name").val();
+		unit_data.characters.user_id = <?= $this->session->userdata("id"); ?>;
+
+		controllerPost("unit/recruit", {"unit": unit_data}, function(data) {
+			if(data.status === "okay") {
+				$(String.format("#name-{0}", id)).html(data.unit_name);
+				$(String.format("#unit-{0}", id)).removeAttr("onClick");
+				$("#recruit").html("Recruited!").prop("disabled", true);
+				$(String.format("#recruit-status-{0}", id)).html("Status: In party").show();
+			} else {
+				$("#recruit").html(data.error).prop("disabled", true);
+			}
+		}, "json");
+	});
 });
 function selectUnit(id) {
 	var unit = units.get(id);
 
 	$("#column-2").show();
+	$("#id").val(id);
 	$("#unit-name").val(unit.name);
 	$("#column-2 .sprite").css("background-image", String.format("url({0})", unit.sprite("retreat")));
-	$("#hp").html(unit.max_hp);
 	$("#level").html(unit.level);
+	$("#hp").html(unit.max_hp);
 	$("#atk").html(unit.attack);
 	$("#def").html(unit.defense);
 	$("#evade").html(unit.evasion);
 	$("#spd").html(unit.speed);
+	$("#recruit").html("Recruit").prop("disabled", false);
 }
 </script> 
 </html>
