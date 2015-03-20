b7e1178b53374f68d8eafbc4e82436198d251707
not-jay <jaysonsona@tfwno.gf>
2015-03-20 10:11:55 +0800

    Fix base_url

--- application/views/dungeon/dungeon_view.php
+++ application/views/dungeon/dungeon_view.php
@@ -1,95 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Dungeon Crawler Mini-RPG</title>
-	<?= link_tag("css/bootstrap.min.css"); ?>
-	<?= link_tag("css/bootstrap-theme.min.css"); ?>
-	<?= link_tag("css/rpg.css"); ?>
-	<?= link_tag("css/battle.css"); ?>
-	<?= link_tag("css/misc.css"); ?>
+    <meta charset="utf-8">
+    <title>Dungeon Crawler Mini-RPG</title>
+    <?= link_tag("css/bootstrap.min.css"); ?>
+    <?= link_tag("css/bootstrap-theme.min.css"); ?>
+    <?= link_tag("css/rpg.css"); ?>
+    <?= link_tag("css/battle.css"); ?>
+    <?= link_tag("css/misc.css"); ?>
 </head>
 <body>
-	<div class="center-outer"><div class="center-middle">
-		<div class="center-content center-text field">
-			<div class="two-column pull-left enemies">
-				<div id="enemy-field-0" class="field-sprite flip"></div>
-				<div id="enemy-field-1" class="field-sprite flip"></div>
-				<div id="enemy-field-2" class="field-sprite flip"></div>
-				<div id="enemy-field-3" class="field-sprite flip"></div>
-			</div>
-			<div class="two-column pull-left player">
-				<div id="player-field-0" class="field-sprite"></div>
-				<div id="player-field-1" class="field-sprite"></div>
-				<div id="player-field-2" class="field-sprite"></div>
-				<div id="player-field-3" class="field-sprite"></div>
-			</div>
-		</div>
-		<div class="center-content battle-ui">
-			<div class="ui-left pull-left">
-				<button id="fight" class="btn btn-primary">Fight</button>
-				<button id="run" class="btn btn-primary">Run Away</button>
-			</div>
-			<div class="ui-right pull-left">
-				<div id="party-0" class="media party-slot">
-					<div class="media-left media-middle">
-						<div class="media-object avatar"></div>
-					</div>
-					<div class="media-body status">
-						<div class="name"></div>
-						<div class="health"></div>
-						<div class="ct progress">
-							<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-						</div>
-					</div>
-				</div>
-				<div id="party-1" class="media party-slot">
-					<div class="media-left media-middle">
-						<div class="media-object avatar"></div>
-					</div>
-					<div class="media-body status">
-						<div class="name"></div>
-						<div class="health"></div>
-						<div class="ct progress">
-							<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-						</div>
-					</div>
-				</div>
-				<div id="party-2" class="media party-slot">
-					<div class="media-left media-middle">
-						<div class="media-object avatar"></div>
-					</div>
-					<div class="media-body status">
-						<div class="name"></div>
-						<div class="health"></div>
-						<div class="ct progress">
-							<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-						</div>
-					</div>
-				</div>
-				<div id="party-3" class="media party-slot">
-					<div class="media-left media-middle">
-						<div class="media-object avatar"></div>
-					</div>
-					<div class="media-body status">
-						<div class="name"></div>
-						<div class="health"></div>
-						<div class="ct progress">
-							<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div></div>
-	<div class="modal fade" id="modal" tabIndex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal">&times;</button>
-			<h4 class="modal-title"></h4>
-		</div>
-		<div class="modal-body"></div>
-		<div class="modal-footer"></div>
-	</div></div></div>
+    <div class="center-outer"><div class="center-middle">
+        <div class="center-content center-text field">
+            <div class="two-column pull-left enemies">
+                <div id="enemy-field-0" class="field-sprite flip"></div>
+                <div id="enemy-field-1" class="field-sprite flip"></div>
+                <div id="enemy-field-2" class="field-sprite flip"></div>
+                <div id="enemy-field-3" class="field-sprite flip"></div>
+            </div>
+            <div class="two-column pull-left player">
+                <div id="player-field-0" class="field-sprite"></div>
+                <div id="player-field-1" class="field-sprite"></div>
+                <div id="player-field-2" class="field-sprite"></div>
+                <div id="player-field-3" class="field-sprite"></div>
+            </div>
+        </div>
+        <div class="center-content battle-ui">
+            <div class="ui-left pull-left">
+                <button id="fight" class="btn btn-primary">Fight</button>
+                <button id="run" class="btn btn-primary">Run Away</button>
+            </div>
+            <div class="ui-right pull-left">
+                <div id="party-0" class="media party-slot">
+                    <div class="media-left media-middle">
+                        <div class="media-object avatar"></div>
+                    </div>
+                    <div class="media-body status">
+                        <div class="name"></div>
+                        <div class="health"></div>
+                        <div class="ct progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="party-1" class="media party-slot">
+                    <div class="media-left media-middle">
+                        <div class="media-object avatar"></div>
+                    </div>
+                    <div class="media-body status">
+                        <div class="name"></div>
+                        <div class="health"></div>
+                        <div class="ct progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="party-2" class="media party-slot">
+                    <div class="media-left media-middle">
+                        <div class="media-object avatar"></div>
+                    </div>
+                    <div class="media-body status">
+                        <div class="name"></div>
+                        <div class="health"></div>
+                        <div class="ct progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="party-3" class="media party-slot">
+                    <div class="media-left media-middle">
+                        <div class="media-object avatar"></div>
+                    </div>
+                    <div class="media-body status">
+                        <div class="name"></div>
+                        <div class="health"></div>
+                        <div class="ct progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div></div>
+    <div class="modal fade" id="modal" tabIndex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title"></h4>
+        </div>
+        <div class="modal-body"></div>
+        <div class="modal-footer"></div>
+    </div></div></div>
 </body>
 <script type="text/javascript" src="<?= base_url('js/jquery.min.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('js/bootstrap.min.js'); ?>"></script>
@@ -99,41 +99,41 @@
 <script type="text/javascript" src="<?= base_url('js/battle.js'); ?>"></script>
 <script type="text/javascript">
 var battle, uiupdate,
-	stepupdate, endupdate;
+    stepupdate, endupdate;
 $(document).ready(function () {
-	initBaseURL("429/final_project/");
-	
-	battle = new Battle();
-	battle.init(<?= $this->session->userdata("id"); ?>);
-	battle.setUI({
-		"fp": "player-field-{0}",
-		"sp": "party-{0}",
-		"fe": "enemy-field-{0}",
-		"mo": "modal"
-	});
+    initBaseURL("");
 
-	$("#run").click(function() {
-		battle.issue("retreat");
-	});
-	$("#fight").click(function() {
-		battle.issue("attack");
-	});
-	$("#modal").on("hidden.bs.modal", function(e) {
-		$(location).attr("href", "town");
-	});
+    battle = new Battle();
+    battle.init(<?= $this->session->userdata("id"); ?>);
+    battle.setUI({
+        "fp": "player-field-{0}",
+        "sp": "party-{0}",
+        "fe": "enemy-field-{0}",
+        "mo": "modal"
+    });
 
-	uiupdate = setInterval(battle.updateUI, 100);
-	stepupdate = setInterval(battle.step, 1000);
-	endupdate = setInterval(endBattle, 1500);
+    $("#run").click(function() {
+        battle.issue("retreat");
+    });
+    $("#fight").click(function() {
+        battle.issue("attack");
+    });
+    $("#modal").on("hidden.bs.modal", function(e) {
+        $(location).attr("href", "town");
+    });
+
+    uiupdate = setInterval(battle.updateUI, 100);
+    stepupdate = setInterval(battle.step, 1000);
+    endupdate = setInterval(endBattle, 1500);
 });
 function endBattle() {
-	if(battle.hasEnded()) {
-		battle.postmortem();
-		battle.saveChanges();
-		clearInterval(uiupdate);
-		clearInterval(stepupdate);
-		clearInterval(endupdate);
-	}
+    if(battle.hasEnded()) {
+        battle.postmortem();
+        battle.saveChanges();
+        clearInterval(uiupdate);
+        clearInterval(stepupdate);
+        clearInterval(endupdate);
+    }
 }
 </script>
 </html>
--- application/views/party/party_view.php
+++ application/views/party/party_view.php
@@ -1,54 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Dungeon Crawler Mini-RPG</title>
-	<?= link_tag("css/misc.css"); ?>
-	<?= link_tag("css/rpg-ui.css"); ?>
-	<?= link_tag("css/bootstrap.min.css"); ?>
-	<?= link_tag("css/bootstrap-theme.min.css"); ?>
+    <meta charset="utf-8">
+    <title>Dungeon Crawler Mini-RPG</title>
+    <?= link_tag("css/misc.css"); ?>
+    <?= link_tag("css/rpg-ui.css"); ?>
+    <?= link_tag("css/bootstrap.min.css"); ?>
+    <?= link_tag("css/bootstrap-theme.min.css"); ?>
 </head>
 <body>
-	<div class="center-outer"><div class="center-middle">
-		<div class="center-content">
-			<div class="two-column pull-left units ff7">
-				<div class="center-text title"><h1>Party</h1></div>
-				<?php for($i = 0; $i < count($units); $i++): ?>
-				<div id="unit-<?= $i ?>" class="media unit-slot" onClick="selectUnit(<?= $i; ?>);">
-					<div class="media-left media-middle">
-						<div class="media-object avatar">
-							<img src="<?= base_url('img/avatars/'.$units[$i]->sprite_name.'.gif'); ?>" />
-						</div>
-					</div>
-					<div class="media-body media-middle">
-						<div id="name-<?= $i ?>"><?= $units[$i]->name; ?></div>
-						<div id="delete-status-<?= $i ?>"></div>
-					</div>
-				</div>
-				<?php endfor; ?>
-			</div>
-			<div id="column-2" class="two-column pull-left stats ff7">
-				<div class="center-text title"><h1 id="unit-name"></h1></div>
-				<div class="sprite center-block"></div>
-				<div class="center-block center-content unit-stats">
-					<div class="spring">
-						<input id="id" type="hidden">
-						Level: <span id="level"></span><br>
-						Exp: <span id="exp"></span>&nbsp;<span id="next-lvl"></span><br>
-						HP: <span id="hp"></span> / <span id="max_hp"></span><br>
-						Attack: <span id="atk"></span><br>
-						Defense: <span id="def"></span><br>
-						Evasion: <span id="evade"></span><br>
-						Speed: <span id="spd"></span><br>
-					</div>
-					<div class="spring">
-						<button id="heal" class="btn btn-default center-block">Heal</button><br>
-						<button id="dismiss" class="btn btn-danger center-block">Dismiss</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div></div>
+    <div class="center-outer"><div class="center-middle">
+        <div class="center-content">
+            <div class="two-column pull-left units ff7">
+                <div class="center-text title"><h1>Party</h1></div>
+                <?php for($i = 0; $i < count($units); $i++): ?>
+                <div id="unit-<?= $i ?>" class="media unit-slot" onClick="selectUnit(<?= $i; ?>);">
+                    <div class="media-left media-middle">
+                        <div class="media-object avatar">
+                            <img src="<?= base_url('img/avatars/'.$units[$i]->sprite_name.'.gif'); ?>" />
+                        </div>
+                    </div>
+                    <div class="media-body media-middle">
+                        <div id="name-<?= $i ?>"><?= $units[$i]->name; ?></div>
+                        <div id="delete-status-<?= $i ?>"></div>
+                    </div>
+                </div>
+                <?php endfor; ?>
+            </div>
+            <div id="column-2" class="two-column pull-left stats ff7">
+                <div class="center-text title"><h1 id="unit-name"></h1></div>
+                <div class="sprite center-block"></div>
+                <div class="center-block center-content unit-stats">
+                    <div class="spring">
+                        <input id="id" type="hidden">
+                        Level: <span id="level"></span><br>
+                        Exp: <span id="exp"></span>&nbsp;<span id="next-lvl"></span><br>
+                        HP: <span id="hp"></span> / <span id="max_hp"></span><br>
+                        Attack: <span id="atk"></span><br>
+                        Defense: <span id="def"></span><br>
+                        Evasion: <span id="evade"></span><br>
+                        Speed: <span id="spd"></span><br>
+                    </div>
+                    <div class="spring">
+                        <button id="heal" class="btn btn-default center-block">Heal</button><br>
+                        <button id="dismiss" class="btn btn-danger center-block">Dismiss</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div></div>
 </body>
 <script type="text/javascript" src="<?= base_url('js/jquery.min.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('js/bootstrap.min.js'); ?>"></script>
@@ -58,57 +58,57 @@
 <script type="text/javascript">
 var units;
 $(document).ready(function() {
-	var i, length, ave, unit;
-	units = new Party();
-	initBaseURL("429/final_project/");
+    var i, length, ave, unit;
+    units = new Party();
+    initBaseURL("");
 
-	units.addAll(Unit.createAll(JSON.parse('<?= json_encode($units); ?>')));
-	
-	$("#heal").click(function() {
-		var id = $("#id").val(),
-			unit = units.get(id);
+    units.addAll(Unit.createAll(JSON.parse('<?= json_encode($units); ?>')));
 
-		unit.hp = unit.max_hp;
+    $("#heal").click(function() {
+        var id = $("#id").val(),
+            unit = units.get(id);
 
-		controllerPost("unit/update", {
-			"units": [unit.jsonify()]
-		}, function(data) {
-			selectUnit(id);
-		});
-	});
+        unit.hp = unit.max_hp;
 
-	$("#dismiss").click(function() {
-		var id = $("#id").val();
-			unit = units.get(id);
+        controllerPost("unit/update", {
+            "units": [unit.jsonify()]
+        }, function(data) {
+            selectUnit(id);
+        });
+    });
 
-		unit.hp = 0; //kill unit
+    $("#dismiss").click(function() {
+        var id = $("#id").val();
+            unit = units.get(id);
 
-		controllerPost("unit/update", {
-			"units": [unit.jsonify()]
-		}, function(data) {
-			$(String.format("#unit-{0}", id)).removeAttr("onClick");
-			$(String.format("#delete-status-{0}", id)).html("Status: Dismissed");
-		});
-	});
+        unit.hp = 0; //kill unit
+
+        controllerPost("unit/update", {
+            "units": [unit.jsonify()]
+        }, function(data) {
+            $(String.format("#unit-{0}", id)).removeAttr("onClick");
+            $(String.format("#delete-status-{0}", id)).html("Status: Dismissed");
+        });
+    });
 });
 function selectUnit(id) {
-	var unit = units.get(id),
-		next = controller("exp/get", "int", unit.level + 1) - unit.exp;
+    var unit = units.get(id),
+        next = controller("exp/get", "int", unit.level + 1) - unit.exp;
 
-	$("#column-2").show();
-	$("#id").val(id);
-	$("#unit-name").html(unit.name);
-	$("#column-2 .sprite").css("background-image", String.format("url({0})", unit.sprite("retreat")));
-	$("#level").html(unit.level);
-	$("#exp").html(unit.exp);
-	$("#next-lvl").html(String.format("({0} to next level)", next));
-	$("#hp").html(unit.hp);
-	$("#max_hp").html(unit.max_hp);
-	$("#atk").html(unit.attack);
-	$("#def").html(unit.defense);
-	$("#evade").html(unit.evasion);
-	$("#spd").html(unit.speed);
-	$("#heal").prop("disabled", (unit.hp === unit.max_hp));
+    $("#column-2").show();
+    $("#id").val(id);
+    $("#unit-name").html(unit.name);
+    $("#column-2 .sprite").css("background-image", String.format("url({0})", unit.sprite("retreat")));
+    $("#level").html(unit.level);
+    $("#exp").html(unit.exp);
+    $("#next-lvl").html(String.format("({0} to next level)", next));
+    $("#hp").html(unit.hp);
+    $("#max_hp").html(unit.max_hp);
+    $("#atk").html(unit.attack);
+    $("#def").html(unit.defense);
+    $("#evade").html(unit.evasion);
+    $("#spd").html(unit.speed);
+    $("#heal").prop("disabled", (unit.hp === unit.max_hp));
 }
-</script> 
+</script>
 </html>
